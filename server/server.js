eaa9a4c7965a6ab30c570d854ca9135b0681a074
Kumar Sambhav jain <30227278+samwebber11@users.noreply.github.com>
2018-03-28 19:59:44 +0530

    Add Data

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 const path=require('path');
 const express=require('express');
-
+const http=require('http');
+const socketIO=require('socket.io');
 const finalPath=path.join(__dirname , '../client');
 console.log(finalPath);
 var port=process.env.PORT||3000;
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
 // app.set('view engine','html');
 // app.engine('html',require('hbs').__express);
 // console.log(port);
 app.use(express.static(finalPath));
-app.listen(port,()=>
+// app.get('/',(res,req)=>
+// {
+// 	res.sendFile(`finalPath + 'one.html'`);
+// })
+
+io.on('connection',(socket)=>
+{
+	console.log('New User generated');
+	socket.on('disconnect',()=>
+	{
+		console.log('Disconnected');
+	});
+});
+
+server.listen(port,()=>
 {
 	console.log(`Starting port ${port}`);
 });
