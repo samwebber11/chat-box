6915c1c4b89dbf539e770bcfa48fa24de20d2423
Kumar Sambhav jain <30227278+samwebber11@users.noreply.github.com>
2018-04-06 22:59:15 +0530

    new goons

---
+++ server/message.js
@@ -0,0 +1,11 @@
+var generateMsg=(from,text)=>
+{
+	return 
+	{
+		from,
+		text,
+		createdAt: new Date().getTime()
+	}
+};
+
+module.exports={generateMsg};
---
+++ server/watch.test.js
@@ -0,0 +1,11 @@
+var expect=require('expect');
+
+var {generateMsg}=require('/message.js');
+
+describe('generateMsg',()=>
+{
+	it('should generate a msg',()=>
+	{
+		var res=
+	});
+});
